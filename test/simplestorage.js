f57fd88df6de4738b4955cededb81b8efd8f5913
Max Goodman <max@maxgrok.com>
2020-01-30 06:12:29 -0500

    deleting template files and index(1).html

--- test/simplestorage.js
+++ test/simplestorage.js
@@ -1,15 +1,52 @@
-const SimpleStorage = artifacts.require("./SimpleStorage.sol");
+const ProofOfExistence = artifacts.require("./ProofOfExistence.sol");
 
-contract("SimpleStorage", accounts => {
-  it("...should store the value 89.", async () => {
-    const simpleStorageInstance = await SimpleStorage.deployed();
+contract("ProofOfExistence", accounts => {
+  it("...should notarize 'magic'.", async () => {
+    const proofOfExistenceInstance = await ProofOfExistence.deployed();
 
-    // Set value of 89
-    await simpleStorageInstance.set(89, { from: accounts[0] });
+    // Set value of "magic"
+    await proofOfExistenceInstance.notarize("magic", { from: accounts[0] });
 
     // Get stored value
-    const storedData = await simpleStorageInstance.get.call();
+    const magic = await proofOfExistenceInstance.checkDocument("magic");
 
-    assert.equal(storedData, 89, "The value 89 was not stored.");
+    assert.equal(magic, true, "The keccak256 hash of the document 'magic' was not stored.");
+  });
+
+  it("...should be able to store and have proofs for multiple documents", async() => {
+    const proofOfExistenceInstance = await ProofOfExistence.deployed();
+    
+    await proofOfExistenceInstance.notarize("magic", { from: accounts[0] });
+
+    await proofOfExistenceInstance.notarize("numero duo", { from: accounts[0]});
+
+    const magic = await proofOfExistenceInstance.checkDocument("magic");
+
+    const nduo = await proofOfExistenceInstance.checkDocument("numero duo");
+
+    assert.equal(magic, true, "The keccak256 hash of the document 'magic'.");
+    assert.equal(nduo, true, "document 'numero duo' were not stored.");
+  });
+  
+  it("calculates the keccak256 proof accurately for the document", async() =>{
+    const proofOfExistenceInstance = await ProofOfExistence.deployed();
+    
+    keccak256 = require('js-sha3').keccak256;
+    const document = "magic";
+    const keccak = await proofOfExistenceInstance.proofFor(document);
+
+    assert.equal(keccak, "0x" + keccak256(document) , "does not provide proof for document");
+  });
+  // write three more tests
+  it("", async()=>{
+
+  });
+
+  it("", async()=>{
+
+  });
+
+  it("", async()=>{
+
   });
 });
